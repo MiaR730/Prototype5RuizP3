97291593d636b5f60f190855df11998c9880e8d5
MiaR730 <723420@student.dusd.net>
2024-04-30 11:42:11 -0700

    UI texts game overover and restart.

--- Prototype5P3Ruiz/Assets/Scripts/Target.cs
+++ Prototype5P3Ruiz/Assets/Scripts/Target.cs
@@ -12,6 +12,8 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -2;
     private GameManager gameManager;
+    public int pointValue;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -49,12 +51,20 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-        Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        if (gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 }
